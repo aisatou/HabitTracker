098cc94246bbd5ab4010f471ee2e36aed40db9bf
github username <github email address>
2018-06-18 18:06:46 -0400

    addin tings

---
+++ db/migrate/20180618212631_create_habits.rb
@@ -0,0 +1,11 @@
+class CreateHabits < ActiveRecord::Migration[5.2]
+  def change
+    create_table :habits do |t|
+      t.string :name
+      t.text :description
+      t.string :category
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20180618215117_create_schedules.rb
@@ -0,0 +1,12 @@
+class CreateSchedules < ActiveRecord::Migration[5.2]
+  def change
+    create_table :schedules do |t|
+      t.integer :time
+      t.string :frequency
+      t.datetime :start_date
+      t.datetime :end_date
+
+      t.timestamps
+    end
+  end
+end
