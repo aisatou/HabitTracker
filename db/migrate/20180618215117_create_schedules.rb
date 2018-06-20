683dc17914ea162158a9c6a56cf7a3d8091f06bb
github username <github email address>
2018-06-20 10:29:08 -0400

    removed db

--- db/migrate/20180618215117_create_schedules.rb
+++ db/migrate/20180618215117_create_schedules.rb
@@ -3,9 +3,8 @@ class CreateSchedules < ActiveRecord::Migration[5.2]
     create_table :schedules do |t|
       t.integer :time
       t.integer :user_id
+      t.integer :habit_id
       t.string :frequency
-      t.datetime :start_date
-      t.datetime :end_date
 
       t.timestamps
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Habit.create(name: "Yoga Poses", description: "Did you spend at least 10 minutes, ahem?")
+Habit.create(name: "Drink some water", description: "Get them 8-10 glasses goin'! Chug!")
+Habit.create(name: "Call mom or dad", description: "You know you want toooooo.")
+Habit.create(name: "Tidy the house", description: "You got this!")
+Habit.create(name: "Sleep before 11 pm", description: "It's hard, but you got this!")
+
+User.create(name: "Aisha")
+User.create(name: "Roniece")
+User.create(name: "Hana")
+User.create(name: "Inayah")
+User.create(name: "Soraya")
+User.create(name: "Aliya")
+User.create(name: "Sarah")
+
+Schedule.create(time: "Yoga Poses", user_id: 1, habit_id: 1, frequency: "Weekly")
+Schedule.create(time: "Drink some water", user_id: 2, habit_id: 2, frequency: "Daily")
+Schedule.create(time: "Call mom or dad", user_id: 3, habit_id: 3, frequency: "Daily")
+Schedule.create(time: "Tidy the house", user_id: 4, habit_id: 4, frequency: "Weekly")
+Schedule.create(time: "Sleep before 11 pm", user_id: 5, habit_id: 5, frequency: "Daily")
