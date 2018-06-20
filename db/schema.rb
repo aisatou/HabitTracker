4e8f879d7efde0835afeb74cf1181f6c0e884e98
github username <github email address>
2018-06-20 11:34:18 -0400

    adds schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_18_215117) do
+ActiveRecord::.define(version: 2018_06_18_215117) do
 
   create_table "habits", force: :cascade do |t|
     t.string "name"
