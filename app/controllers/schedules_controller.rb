e5b58e112d85613fff78907ee4f9b42b14ee84fe
github username <github email address>
2018-06-20 14:35:57 -0400

    form issues

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,20 +1,23 @@
-class ScheduleController < ApplicationController
+class SchedulesController < ApplicationController
   def new
     @schedule = Schedule.new
+    @users = User.all
     @habits = Habit.all
   end
 
   def create
-    @schedule = Schedule.new(schedule_params)
-    if @schedule.valid?
-    @schedule.save
-    else
-    render :new
-    end
+    @schedule = Schedule.create(schedule_params)
+    redirect_to(@schedule)
+    # #if @schedule.valid?
+    # @schedule.save
+    # else
+    # render :new
+    # end
   end
 
   def show
-    @schedule = Schedule.find(@schedule.id)
+    byebug
+    @schedule = Schedule.find(params[:id])
   end
 
   def update
@@ -35,7 +38,8 @@ class ScheduleController < ApplicationController
 
   private
     def schedule_params
-      params.require(:schedule).permit(:user_id, :habit_id)
+      # params.require(:schedule).permit(:user_id, :habit_id)
+      params.require(:schedule).permit(:habit_id, :user_id, :frequency, :time, :start_date, :end_dates)
     end
 
 end
