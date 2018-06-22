b0329ce1284f3b36d81b5ff993a1dcec32cce1dc
github username <github email address>
2018-06-22 12:40:22 -0400

    fixed an error in user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,6 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-  has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
 
   # Returns the hash digest of the given string.
